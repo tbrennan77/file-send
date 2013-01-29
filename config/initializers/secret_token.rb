e12dc0a6cf99e572031e0743ac4687198ca45eea
Dillon Hafer <dh@dillonhafer.com>
2013-01-29 16:41:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BwUpload::Application.config.secret_token = '28f0145368177716a76da002641c6685d83dea3f060c1d6f68d235bda0f2143661480627f850262ca5388510403e279a41b82da28ec89169a9d88db73679c825'

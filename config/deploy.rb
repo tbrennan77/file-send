15078d58b558e6ce8f7cd7c51dc96376da170ad4
Dillon Hafer <dh@dillonhafer.com>
2013-01-30 13:53:29 -0500

    Updated deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :user, "boondoc4"
 
 after "deploy:update_code" do
   #run "ln -s #{shared_path}/database.yml #{release_path}/config/database.yml"
-  #run "rm -rf /home8/boondoc4/public_html/pipeline_cms/"
+  run "rm -rf /home8/boondoc4/public_html/bw_send/"
   run "ln -s #{release_path}/public /home8/boondoc4/public_html/bw_send"
   #run "ln -s #{shared_path}/index.html ~/public_html/pipeline_cms/index.html"
   #run "cd #{release_path} ; RAILS_ENV=production bundle exec rake assets:precompile --trace"

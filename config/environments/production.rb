70bcd8846ad6fd90b80e6334f64e9e3e587d1681
Dillon Hafer <dh@dillonhafer.com>
2013-01-30 18:05:23 -0500

    added smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,13 @@ BwUpload::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "box695.bluehost.com",
+  :port                 => 465,
+  :domain               => 'send.boondockwalker.com',
+  :user_name            => 'no-reply+send.boondockwalker.com',
+  :password             => '1qaz!QAZ',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 end

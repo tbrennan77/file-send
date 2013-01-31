58e7f835af4f8a49352cd2557ae68d4d26aca069
Dillon Hafer <dh@dillonhafer.com>
2013-01-31 17:07:21 -0500

    Updated report email and various UI changes

--- app/models/bw_file.rb
+++ app/models/bw_file.rb
@@ -7,4 +7,17 @@ class BwFile < ActiveRecord::Base
   validates :email_ids, :presence => { :message => "You should send the file to at least one person." }
   validates :binary_file, :presence => { :message => "It looks like there's no file attached." }
 
+  def self.clean_up
+    messages = {:file => []}    
+    bw_files = BwFile.where("created_at::date < '#{Date.today.advance(:days => -30)}'::date") || []
+    bw_files.each do |bw|
+      message[:file][] << bw.binary_file.original_file_name
+      bw.binary_file = nil
+      bw.save
+      bw.destroy
+    end
+
+    Notifier.clean_up_email(messages) unless messages[:file].empty?
+  end
+
 end

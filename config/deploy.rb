b2bc833a67a0ad2138a48d480ef76f043d2b1a8a
Dillon Hafer <dh@dillonhafer.com>
2013-02-14 19:36:27 -0500

    Updated deploy and fix js error

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ after "deploy:update_code" do
   #run "ln -s #{shared_path}/database.yml #{release_path}/config/database.yml"
   #run "ln -s #{shared_path}/1001303134.pem #{release_path}/config/1001303134.pem"
   #run "ln -s #{shared_path}/videos #{release_path}/public/videos"
-  #run "cd #{release_path} ; RAILS_ENV=production bundle exec rake assets:precompile --trace"
+  run "cd #{release_path} ; RAILS_ENV=production bundle exec rake assets:precompile --trace"
   # Fix group so cached javascripts and stylesheets can be generated
   #run "chgrp FBCS #{release_path}/public/javascripts"
   #run "chgrp FBCS #{release_path}/public/stylesheets"

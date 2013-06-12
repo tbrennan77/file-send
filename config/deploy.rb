55c56612ec818db7ecbed36cad7e4020c79b7a9a
Dillon Hafer <dh@dillonhafer.com>
2013-06-12 15:31:11 -0400

    Updated for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,29 +5,27 @@ set :application, "bw_upload"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-role :app, "50.116.23.78"
-role :web, "50.116.23.78"
-role :db, "50.116.23.78", :primary => true
+role :app, "boondockwalkerstaging.com"
+role :web, "boondockwalkerstaging.com"
+role :db, "boondockwalkerstaging.com", :primary => true
 
-set :deploy_to, "/var/www/bw_send"
+set :deploy_to, "/home6/boondoc7/rails_apps/bw_send"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, :git
-set :repository, "boondoc4@redmine.boondockstaging.com:/home8/boondoc4/www/git/bw_send.git"
+set :repository, "git@git.boondockwalker.com:boondock/file-sharing.git"
 set :branch, "master"
 
-set :user, "bw_send"
+set :user, "boondoc7"
 
 after "deploy:update_code" do
-  #run "ln -s #{shared_path}/database.yml #{release_path}/config/database.yml"
-  #run "ln -s #{shared_path}/1001303134.pem #{release_path}/config/1001303134.pem"
-  #run "ln -s #{shared_path}/videos #{release_path}/public/videos"
+  run "ln -s #{shared_path}/database.yml #{release_path}/config/database.yml"
+  run "ln -s #{release_path}/public /home6/boondoc7/public_html/send"
   run "cd #{release_path} ; RAILS_ENV=production bundle exec rake assets:precompile --trace"
-  # Fix group so cached javascripts and stylesheets can be generated
-  #run "chgrp FBCS #{release_path}/public/javascripts"
-  #run "chgrp FBCS #{release_path}/public/stylesheets"
 end
+
+
 
 namespace :deploy do
   task :start, :roles => :app do
@@ -36,9 +34,5 @@ namespace :deploy do
   
   task :restart, :roles => :app do
     run "touch #{File.join(current_path, "tmp", "restart.txt")}"
-  end
-
-  task :precompile do
-    run "cd #{current_path} ; RAILS_ENV=production bundle exec rake assets:precompile --trace"
   end
 end

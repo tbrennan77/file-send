af72ccc8f43679cccda5f671512339fd36bf7db0
Dillon Hafer <dh@dillonhafer.com>
2014-05-21 15:24:52 -0400

    Adds xframe

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   before_filter :authenticate, :only => %w{admin}
+  after_action :allow_iframe
 
   def index
     @bw_file = BwFile.new
@@ -10,5 +11,11 @@ class HomeController < ApplicationController
     @bw_file = BwFile.new
     @bw_file.bw_attachments.build
     @files = BwFile.order('id DESC')
+  end
+
+  private
+
+  def allow_iframe
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM http://bw.boondockwalkerstaging.com'
   end
 end
